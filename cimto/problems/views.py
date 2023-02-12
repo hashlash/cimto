58ba4d1998d6138aab28cda99ae515238ea2f0bf
hashlash <git@hashlash.dev>
2023-02-12 00:21:43 +0000

    Add problem endpoint permission check

--- cimto/problems/views.py
+++ cimto/problems/views.py
@@ -1,9 +1,11 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from cimto.problems.models import Problem
 from cimto.problems.serializers import ProblemSerializer
 
 
 class ProblemViewset(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Problem.objects.all()
     serializer_class = ProblemSerializer
