95f8284401fc0af9d4f26c7b07bc7d7c631bda6a
hashlash <git@hashlash.dev>
2023-02-15 04:34:48 +0000

    Add problem timestamp

--- cimto/problems/models.py
+++ cimto/problems/models.py
@@ -5,6 +5,8 @@ class Problem(models.Model):
     title = models.CharField(max_length=255, blank=True)
     description = models.TextField()
     answer_key = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         if self.title:
