0568617a86777d39979951c921705e6bbdc69e9b
hashlash <muh.ashlah@gmail.com>
2023-06-17 08:09:14 +0000

    Add whitenoise for fly.io deploy

--- cimto/settings/production.py
+++ cimto/settings/production.py
@@ -10,3 +10,11 @@ CSRF_TRUSTED_ORIGINS = ['https://cimto.fly.dev']
 
 if 'DATABASE_URL' in os.environ:
     DATABASES['default'] = dj_database_url.config()  # noqa: F405
+
+
+# Whitenoise setting
+# https://whitenoise.readthedocs.io/en/latest/
+
+# put whitenoise after security middleware
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')  # noqa: F405
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
